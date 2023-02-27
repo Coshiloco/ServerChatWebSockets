4b6b1a902f9de74f66f6474dceed0763f9d7b00c
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2023-02-27 17:25:35 +0100

    UltimoCambio

--- src/main/kotlin/com/example/Channel.kt
+++ src/main/kotlin/com/example/Channel.kt
@@ -5,6 +5,7 @@ import java.util.*
 
 const val DEFAULT_CHANNEL_NAME = "general"
 
+
 class Channel(val name: String) {
     val users = Collections.synchronizedSet<Connection>(LinkedHashSet())
     val channels = mutableMapOf<String, Channel>()
